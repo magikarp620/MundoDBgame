fcb849766e078de87baa57c2f4326a41f6527638
magikarp620 <98373624+magikarp620@users.noreply.github.com>
2023-01-18 22:57:58 -0800

    move

--- public/game.js
+++ public/game.js
@@ -1,6 +1,7 @@
 const config = {
+    physics: {default:"arcade"},
     type: Phaser.AUTO,
-    width: 1280,
+    width: 953,
     height: 720,
     scene: {
         preload: preload,
@@ -9,16 +10,34 @@ const config = {
     }
 }
 const game = new Phaser.Game(config)
-
+let char1
+let targetx = 280
+let targety = 250
 function preload () {
     console.log("load")
     this.load.image("background", 'images/baronpit.jpg')
+    this.load.image("char1", "images/character1.webp");
 }
 
 function create () {
-    this.add.image(0,0, 'background')
+    this.input.mouse.disableContextMenu();
+    this.add.image(this.cameras.main.width / 2,this.cameras.main.height / 2, 
+    'background')
+    char1 = this.physics.add.sprite(280, 250, 'char1');
+    char1.setScale(0.08)
+    
 }
 
 function update () {
-
+    const distance = Phaser.Math.Distance.Between(targetx,targety, char1.x, char1.y);
+    
+    if(distance < 3){
+        char1.setVelocity(0,0)
+    }
+    const pointer = this.input.activePointer;
+    if(pointer.rightButtonDown()){
+        targetx = pointer.worldX
+        targety = pointer.worldY
+        this.physics.moveTo(char1,pointer.worldX,pointer.worldY,240)
+    }    
 }
